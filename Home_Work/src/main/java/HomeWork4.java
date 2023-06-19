60495a406d47e65ce070bc55e96e2cb0954cecb2
Norros <Andriyxa_77@mail.ru>
2023-06-20 02:23:42 +0300

    добавлено дз 4

--- Home_Work/src/main/java/HomeWork4.java
+++ Home_Work/src/main/java/HomeWork4.java
@@ -11,25 +11,23 @@ public class HomeWork4 {
 
     public static void main(String[] args) {
 
-        input_data(surname, name, patronymic, age, gender, keys);
-        print_data(surname, name, patronymic, age, gender, keys);
+        input_data();
+        print_data(keys);
 
         System.out.println("Выберимте действие: q - выход, s - отсортировать по возрасту");
         String data = scaner.nextLine();
         if (data.equals("q")) System.exit(0);
         System.out.println("Сортировка по возрасту");
         System.out.println("__________________");
-        sortdata_age();
+        print_data(sortdata_age());
         System.out.println("Сортировка по возрасту и полу");
         System.out.println("__________________");
-        sortdata_age_and_gender();
+        print_data(sortdata_age_and_gender());
 
 
     }
 
-    static void input_data(ArrayList<String> surname, ArrayList<String> name,
-                           ArrayList<String> patronymic, ArrayList<Integer> age,
-                           ArrayList<String> gender, ArrayList<Integer> keys) {
+    static void input_data() {
 
         System.out.println("Введите Фамилию Имя ОЧество вораст и пол ");
         int count = 0;
@@ -46,48 +44,43 @@ public class HomeWork4 {
         }
     }
 
-    static void print_data(ArrayList<String> surname, ArrayList<String> name,
-                           ArrayList<String> patronymic, ArrayList<Integer> age,
-                           ArrayList<String> gender, ArrayList<Integer> keys) {
+    static void print_data(ArrayList<Integer> keys) {
         for (Integer i : keys) {
             System.out.println(surname.get(i) + " " + name.get(i).toUpperCase().charAt(0)
                     + " " + patronymic.get(i).toUpperCase().charAt(0) + " " + age.get(i) + " " + gender.get(i));
         }
     }
 
-    static void sortdata_age() {
-        ArrayList<Integer> temp_keys = new ArrayList<>();
+    static ArrayList<Integer> sortdata_age() {
+        ArrayList<Integer> sorted_keys = new ArrayList<>();
         ArrayList<Integer> tmp_age = new ArrayList<>();
         tmp_age.addAll(age);
         for (int i = 0; i < age.size(); i++) {
-            temp_keys.add(age.indexOf(Collections.min(tmp_age)));
-            tmp_age.remove(tmp_age.indexOf(Collections.min(tmp_age)));
+            sorted_keys.add(age.indexOf(Collections.min(tmp_age)));
+            tmp_age.remove(tmp_age
+                    .indexOf(Collections
+                            .min(tmp_age)));
         }
-        print_data(surname, name, patronymic, age, gender, temp_keys);
+        return sorted_keys;
     }
 
-    static void sortdata_age_and_gender() {
-        ArrayList<Integer> temp_keys = new ArrayList<>();
-        ArrayList<Integer> tmp_age = new ArrayList<>();
-        tmp_age.addAll(age);
-        for (int i = 0; i < age.size(); i++) {
-            temp_keys.add(age.indexOf(Collections.min(tmp_age)));
-            tmp_age.remove(tmp_age.indexOf(Collections.min(tmp_age)));
-        }
+    static ArrayList<Integer> sortdata_age_and_gender() {
+        ArrayList<Integer> sorted_keys = new ArrayList<>();
+        sorted_keys.addAll(sortdata_age());
         int tmp;
         int k = 0;
-        ArrayList<Integer> temp_keys1 = new ArrayList<>();
-        temp_keys1.addAll(temp_keys);
-        for (int i = 0; i < keys.size()-1; i++) {
-            if (gender.get(temp_keys.get(i)).toLowerCase().equals("ж")) {
-                tmp = temp_keys1.get(k);
-                temp_keys1.remove(k);
-                temp_keys1.add(tmp);
+        ArrayList<Integer> sorted_keys1 = new ArrayList<>();
+        sorted_keys1.addAll(sorted_keys);
+        for (int i = 0; i < keys.size() - 1; i++) {
+            if (gender.get(sorted_keys.get(i)).toLowerCase().equals("ж")) {
+                tmp = sorted_keys1.get(k);
+                sorted_keys1.remove(k);
+                sorted_keys1.add(tmp);
             } else k += 1;
 
         }
 
-        print_data(surname, name, patronymic, age, gender, temp_keys1);
+        return sorted_keys1;
     }
 
 
